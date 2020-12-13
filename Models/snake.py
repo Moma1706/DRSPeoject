dd38d125a63b7b9a4c21e8b2de7d09b8f9bb6840
Moma1706 <57521686+Moma1706@users.noreply.github.com>
2020-12-13 21:59:38 +0100

    Snake movement
    Implementation of snakes movement and fixing the resizing bug

--- Models/snake.py
+++ Models/snake.py
@@ -16,7 +16,7 @@ class Snake(QGraphicsItem):
 
     def __init__(self, parent: object) -> None:
         super(Snake, self).__init__()
-        self.parent = parent.graphicsView
+        self.parent = parent.canvas
         self.particleSize = parent.particleSize
 
         # Random direction at the start
@@ -28,7 +28,6 @@ class Snake(QGraphicsItem):
         width = range(self.particleSize, self.particleSize * 2, self.particleSize)
         height = range(self.particleSize, self.particleSize * 2, self.particleSize)
         self.body = [[width[int(len(width) / 2)], height[int(len(height) / 2)]]]
-     
 
     def paint(self, painter: QPainter, option: QStyleOptionGraphicsItem, widget: QWidget) -> None:
         brush = QBrush(QColor(76, 175, 79), Qt.Dense3Pattern)
@@ -38,5 +37,26 @@ class Snake(QGraphicsItem):
         for i in range(len(self.body)):
             painter.drawRect(self.body[i][0], self.body[i][1], self.particleSize, self.particleSize)
 
+         # Move Snake
+        # Take head and move it around according to the direction
+        head = [sum(x) for x in zip(self.body[0], self.direction)]
+        # Remove the last element of the list
+        self.body.pop()
+        # Insert the new head
+        self.body.insert(0, head)
+
     def boundingRect(self) -> QRectF:
-        return QRectF(0, 0, 400, 400)
+        return QRectF(-400, -335, 900, 700)
+
+    def changeDirection(self, key: int) -> None:
+        """
+        Change the Snake's direction according to the key the user has pressed
+        """
+        if key in [Qt.Key_A, Qt.Key_Left] and self.direction != [self.particleSize, 0]:
+            self.direction = [-self.particleSize, 0]
+        elif key in [Qt.Key_D, Qt.Key_Right] and self.direction != [-self.particleSize, 0]:
+            self.direction = [self.particleSize, 0]
+        elif key in [Qt.Key_S, Qt.Key_Down] and self.direction != [0, -self.particleSize]:
+            self.direction = [0, self.particleSize]
+        elif key in [Qt.Key_W, Qt.Key_Up] and self.direction != [0, self.particleSize]:
+            self.direction = [0, -self.particleSize]
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from PyQt5.QtWidgets import QWidget, QProgressBar, QPushButton, QApplication, QLabel, QMainWindow, QHBoxLayout, \
-    QGraphicsScene, QGraphicsView, QDesktopWidget
-from PyQt5.QtGui import QIcon, QPixmap, QPainter, QKeyEvent, QPen
-
-from threading import Thread
-
 import sys
+
+from PyQt5.QtCore import QBasicTimer, QTime, QTimerEvent
+from PyQt5.QtGui import QIcon, QKeyEvent, QPen
+from PyQt5.QtWidgets import QPushButton, QApplication, QLabel, QMainWindow, QHBoxLayout, \
+    QGraphicsScene, QGraphicsView, QDesktopWidget
+
 from Models.snake import *
-import multiprocessing as mp
-import time
+
 
 class SnakeWindow(QMainWindow):
     def __init__(self):
@@ -16,11 +15,14 @@ class SnakeWindow(QMainWindow):
         self.playing = False
         self.score = 0
         self.particleSize = 10
+        self.speed = 100
+        self.timer = QBasicTimer()
+        self.playtime = QTime()
 
-    #window setup
+        # window setup
         self.setCentralWidget(QWidget())
         self.setWindowTitle("\t\t\t\tTurnSnakeGdxGame")
-        self.resize(1100, 750)
+        self.resize(1120, 750)
 
         self.move(200, 100)
         self.setWindowIcon(QIcon('snake.png'))
@@ -29,33 +31,81 @@ class SnakeWindow(QMainWindow):
         self.setLayout(horizontal)
         self.setStyleSheet("background-image : url(galaxy.jpg);")
 
-    #canvas
+        # canvas
         self.canvas = QGraphicsScene(self)
         self.graphicsView = QGraphicsView(self.canvas, self)
         # Use all the QGraphicsView area
-        self.canvas.setSceneRect(0, 0, self.graphicsView.width(), self.graphicsView.height())
+        self.graphicsView.resize(905, 705)
+        self.canvas.setSceneRect(0, 0, (self.canvas.width()), (self.canvas.height()))
         self.canvas.setBackgroundBrush(QBrush(QColor(52, 56, 56), Qt.SolidPattern))
         self.graphicsView.setScene(self.canvas)
-        self.graphicsView.resize(900, 700)
 
-    # score label setup
-        self.scoreLabel = QLabel(self)
-        self.score = "Press <b>Enter</b> to start game"
-        self.scoreLabel.setText(str(self.score))
-        self.scoreLabel.resize(200, 200)
-        self.scoreLabel.move(950, 10)
-        self.scoreLabel.setStyleSheet("color: white;")
+        # score labels setup
 
-    #next player button
-        #self.centralWidget().layout().addWidget()
+        self.playerLabel1 = QLabel(self)
+        self.player1 = "Player1:"
+        self.playerLabel1.setText(str(self.player1))
+        self.playerLabel1.resize(100, 50)
+        self.playerLabel1.move(930, 15)
+        self.playerLabel1.setStyleSheet("color: white;")
+
+        self.scoreLabel1 = QLabel(self)
+        self.score1 = "Press <b>Enter</b> to start game"
+        self.scoreLabel1.setText(str(self.score1))
+        self.scoreLabel1.resize(100, 50)
+        self.scoreLabel1.move(1000, 15)
+        self.scoreLabel1.setStyleSheet("color: white;")
+
+        self.playerLabel2 = QLabel(self)
+        self.player2 = "Player2:"
+        self.playerLabel2.setText(str(self.player2))
+        self.playerLabel2.resize(100, 50)
+        self.playerLabel2.move(930, 65)
+        self.playerLabel2.setStyleSheet("color: white;")
+
+        self.scoreLabel2 = QLabel(self)
+        self.score2 = "0"
+        self.scoreLabel2.setText(str(self.score2))
+        self.scoreLabel2.resize(100, 50)
+        self.scoreLabel2.move(1000, 65)
+        self.scoreLabel2.setStyleSheet("color: white;")
+
+        self.playerLabel3 = QLabel(self)
+        self.player3 = "Player3:"
+        self.playerLabel3.setText(str(self.player3))
+        self.playerLabel3.resize(100, 50)
+        self.playerLabel3.move(930, 110)
+        self.playerLabel3.setStyleSheet("color: white;")
+
+        self.scoreLabel3 = QLabel(self)
+        self.score3 = "0"
+        self.scoreLabel3.setText(str(self.score3))
+        self.scoreLabel3.resize(100, 50)
+        self.scoreLabel3.move(1000, 110)
+        self.scoreLabel3.setStyleSheet("color: white;")
+
+        self.playerLabel4 = QLabel(self)
+        self.player4 = "Player4:"
+        self.playerLabel4.setText(str(self.player4))
+        self.playerLabel4.resize(100, 50)
+        self.playerLabel4.move(930, 160)
+        self.playerLabel4.setStyleSheet("color: white;")
+
+        self.scoreLabel4 = QLabel(self)
+        self.score4 = "0"
+        self.scoreLabel4.setText(str(self.score4))
+        self.scoreLabel4.resize(100, 50)
+        self.scoreLabel4.move(1000, 160)
+        self.scoreLabel4.setStyleSheet("color: white;")
+
+        # next player button
+        # self.centralWidget().layout().addWidget()
         self.btn = QPushButton('Next Player', self)
         self.btn.setStyleSheet("background-color: purple; color: white;")
 
         self.btn.move(1000, 600)
         self.btn.resize(80, 80)
-        #self.btn.clicked.connect(self.doAction)
         self.drawBorder()
-        self.centerOnScreen()
         self.show()
 
     def startGame(self) -> None:
@@ -66,24 +116,35 @@ class SnakeWindow(QMainWindow):
         self.playing = True
 
         # Reset the score
-        self.score = 0
-        self.scoreLabel.setText(str(self.score))
+        self.score1 = 0
+        self.scoreLabel1.setText(str(self.score1))
+
+        self.speed = 100
+        self.playtime.start()
+        self.timer.start(self.speed, Qt.PreciseTimer, self)
 
         # Add the new Snake object to the canvas
         self.snake = Snake(self)
-
+        self.snake2 = Snake(self)
         self.canvas.addItem(self.snake)
+        self.canvas.addItem(self.snake2)
+
+        # self.snake.setFocus()
 
     def keyPressEvent(self, event: QKeyEvent) -> None:
         start = [Qt.Key_Return, Qt.Key_Enter]
         # Game can be played using Arrow keys and WASD
         directions = [Qt.Key_Left, Qt.Key_A, Qt.Key_Right, Qt.Key_D, Qt.Key_Up, Qt.Key_W, Qt.Key_Down, Qt.Key_S]
 
-        # Starts a new game if not already playingw
+        # Starts a new game if not already playing
         if not self.playing and event.key() in start:
             self.startGame()
 
- # nzm sta
+        # Change the Snake's movement direction
+        if self.playing and event.key() in directions:
+            self.snake.changeDirection(event.key())
+
+    # nzm sta
     def drawBorder(self) -> None:
         """
         Draw a decorative border in the perimeter of the QGraphicsView
@@ -94,15 +155,9 @@ class SnakeWindow(QMainWindow):
         # Change the background color for the object being drawn
         background = QBrush(QColor(0, 95, 107), Qt.Dense3Pattern)
 
-        # [0, 10, 20, 30, ... , self.canvas.width()] with particle size set to 10
-        topBottom = range(0, int(self.canvas.width()), self.particleSize)
-
-        # [10, 20, 30, 40, ... , self.canvas,height() - 10] with particle size set to 10
-        leftRight = range(self.particleSize, int(self.canvas.height()) - self.particleSize, self.particleSize)
-
         size = self.particleSize
-        width = 900 #self.canvas.width()
-        height = 700 #self.canvas.height()
+        width = 900  # self.canvas.width()
+        height = 700  # self.canvas.height()
 
         # Top, Bottom, Left, Right borders (in that order)
         areas = [
@@ -125,9 +180,32 @@ class SnakeWindow(QMainWindow):
         frameGeometry.moveCenter(centerPoint)
         self.move(frameGeometry.topLeft())
 
+    def timerEvent(self, event: QTimerEvent) -> None:
+        """
+        In charge of, in this case, update the game and check the
+        conditions to continue playing, grow, spawn food and special item
+        """
+
+        # Check if the event if from the self.timer
+        if event.timerId() is self.timer.timerId():
+            self.snake.update()
+
+            # Increase the movement speed of the Snake every 60 seconds
+            if self.playtime.elapsed() > 60000:
+                self.playtime.restart()
+                self.speed -= 10
+
+                # Stop and start the timer, there is no method timer.setTime or
+                # the like for changing the timer's speed of refresh
+                self.timer.stop()
+                self.timer.start(self.speed, Qt.PreciseTimer, self)
+
+        else:
+            super(SnakeWindow, self).timerEvent(event)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     app = QApplication([])
     rep = SnakeWindow()
     sys.exit(app.exec_())
-
